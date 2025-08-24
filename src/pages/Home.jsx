9cc83a0c411e2870ada84beafa52c55d29b1f071
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-08-25 01:37:44 +0530

    update Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,49 @@
+import React, { useContext, useState } from "react";
+import withAuth from "../utils/withAuth";
+import { useNavigate } from "react-router-dom";
 import "../App.css";
 import { Button, IconButton, TextField } from "@mui/material";
 import RestoreIcon from "@mui/icons-material/Restore";
+import { AuthContext } from "../contexts/AuthContext";
 
 function HomeComponent() {
+  let navigate = useNavigate();
+  const [meetingCode, setMeetingCode] = useState("");
+
+  const { addToUserHistory } = useContext(AuthContext);
+  let handleJoinVideoCall = async () => {
+    await addToUserHistory(meetingCode);
+    navigate(`/${meetingCode}`);
+  };
+
   return (
     <>
       <div className="navBar">
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <h2>Apna Video Call</h2>
+        <div
+          className="navHeader"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <h2>Voom</h2>
         </div>
 
         <div style={{ display: "flex", alignItems: "center" }}>
-          <IconButton>
+          <IconButton
+            onClick={() => {
+              navigate("/history");
+            }}
+          >
             <RestoreIcon />
           </IconButton>
           <p>History</p>
-          <Button>Logout</Button>
+
+          <Button
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/auth");
+            }}
+          >
+            Logout
+          </Button>
         </div>
       </div>
 
@@ -25,8 +53,15 @@ function HomeComponent() {
             <h2>Providing Quality Video Call Just Like Quality Education</h2>
 
             <div style={{ display: "flex", gap: "10px" }}>
-              <TextField id="outlined-basic" label="Meeting Code" />
-              <Button>Join</Button>
+              <TextField
+                onChange={(e) => setMeetingCode(e.target.value)}
+                id="outlined-basic"
+                label="Meeting Code"
+                variant="outlined"
+              />
+              <Button onClick={handleJoinVideoCall} variant="contained">
+                Join
+              </Button>
             </div>
           </div>
         </div>
@@ -38,4 +73,4 @@ function HomeComponent() {
   );
 }
 
-export default HomeComponent;
+export default withAuth(HomeComponent);
