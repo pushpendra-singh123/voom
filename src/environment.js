012dc8e3d236276cfb52649d624b37abd1e8a3ee
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-09-04 07:49:17 +0530

    environment setup

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-  ? "https://apnacollegebackend.onrender.com"
+  ? "https://voom-opfg.onrender.com"
   : "http://localhost:8000";
 
 export default server;
